066ce58df23ad1c7fc4ba3464ed5aa4589a717fb
Gabriel Duarte <gabrield.developer@gmail.com>
2021-04-02 22:15:10 -0300

    feat: implement image-cropper plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-image-edit';
+  
+  imgChangeEvent: any = '';
+  cropImagePreview: any = '';
+
+  onFileChange(event: any): void {
+    this.imgChangeEvent = event;
+  }
+
+  cropImage(event: ImageCroppedEvent) {
+    this.cropImagePreview = event.base64;
+  }
+
+  imageLoad() {
+
+  }
+
+  initCropper() {
+
+  }
+
+  imageFailed() {
+    
+  }
+
 }
